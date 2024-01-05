0d39be26f4df9f1326d196ff49afee9852caef4b
BuildTools <itsmepapy@gmail.com>
2024-01-05 20:22:11 +0300

    sds

--- main.py
+++ main.py
@@ -1,36 +1,13 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+bot = telebot.TeleBot('6888123864:AAGmaTR-nJA3wZdT3THLrnKUUkfBOlCgINg')
 
-# Define a command handler for the /start command
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! Welcome to the bot.')
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, 'Hello! Welcome to the bot.')
 
-# Define a message handler to echo user messages
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text=update.message.text)
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    bot.send_message(message.chat.id, message.text)
 
-def main():
-    # Create the Updater and pass your bot's token
-    updater = Updater(token='6888123864:AAGmaTR-nJA3wZdT3THLrnKUUkfBOlCgINg', use_context=True)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # Register the command handler for the /start command
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-    # Register the message handler to echo user messages
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
-
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.polling()
